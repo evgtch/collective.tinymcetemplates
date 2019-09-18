085a40320ead495dd0b6ce68ad007a62cd5ef52d
Evgeniya Vetchinkina <evetchinkina@gmail.com>
2019-09-18 21:29:21 +0100

    Add folder title to templatelist returned JSON.

--- collective/tinymcetemplates/browser.py
+++ collective/tinymcetemplates/browser.py
@@ -3,6 +3,8 @@ try:
 except ImportError:
     import simplejson as json
 
+import os
+
 from zope.component import queryUtility
 from zope.publisher.browser import BrowserView
 
@@ -12,6 +14,12 @@ try:
     from Products.ATContentTypes.interfaces.document import IATDocument
 except:
     from Products.ATContentTypes.interface import IATDocument
+
+try:
+    from Products.ATContentTypes.interfaces.folder import IATFolder
+except:
+    from Products.ATContentTypes.interface import IATFolder
+
 from Products.CMFCore.utils import getToolByName
 
 class TemplateList(BrowserView):
@@ -21,7 +29,7 @@ class TemplateList(BrowserView):
         self.request.response.setHeader('Content-Type', 'text/javascript')
         
         registry = queryUtility(IRegistry)
-        templates = []
+        templates = {}
         
         if registry is not None:
             templateDirectories = registry.get('collective.tinymcetemplates.templateLocations', None)
@@ -36,8 +44,12 @@ class TemplateList(BrowserView):
                     if p.startswith('/'):
                         p = p[1:]
                     paths.append("%s/%s" % (portal_path, p,))
-                
+
                 for r in portal_catalog(path=paths, object_provides=IATDocument.__identifier__):
-                    templates.append([r.Title, "%s/getText" % r.getURL(), r.Description])
+                    tp = os.path.dirname(r.getPath())
+                    f = portal_catalog(path=tp, object_provides=IATFolder.__identifier__)
+                    if not templates.has_key(f[0].Title):
+                        templates[f[0].Title] = []
+                    templates[f[0].Title].append([r.Title, "%s/getText" % r.getURL(), r.Description])
         
         return u"var tinyMCETemplateList = %s;" % json.dumps(templates)
